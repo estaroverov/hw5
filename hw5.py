211c6a219c9db80472b5a61c383c4a05c7d7af8f
eduard <you@example.com>
2022-11-21 18:10:57 +0300

    Решены все задачи

--- hw5.py
+++ hw5.py
@@ -23,129 +23,102 @@ print(RemoveWords("Пабв пппп лллабв абвжддд, абв Вва�
 # Создайте программу для игры с конфетами человек против человека.
 
 
-def Step(player, limit=28):
-    step = int(input(f"Игрок {player}, Ваш ход: "))
-    while step <= 0 or step > limit:
-        step = int(input(f"Игрок {player}, Ваш ход: "))
-    return step
 
 
-def FinishGame(numSweets, player, counterSteps, sum1, sum2, bot=True):
-    print("Final")
-    threshold = 56
-    maxSweets = 28
-    while numSweets >= threshold+2:
-        if player == 2:
-            player = 1
-            if bot == True:
-                diff = numSweets - threshold
-                step = diff-2
-            else:
-                step = Step(player)
-            sum1 += step
-        else:
-            player = 2
-            step = Step(player)
-            sum2 += step
-        counterSteps += 1
-        numSweets -= step  # <=58
-        print(counterSteps, "ход. Игрок", player, "взял конфет:", step)
-        print("Осталось конфет:", numSweets)
-    j = 0
-    while numSweets > 0:
-        if j == 0:
-            if numSweets >= threshold+2:  # >=58
-                if player == 1:
-                    player = 2
-                    step = Step(player)
-                    sum2 += step
-                else:
-                    if step == maxSweets:  #
-                        if bot == True:
-                            step = 1  # Останется 29
-                        else:
-                            step = Step(player)
-                    elif step < maxSweets:
-                        if bot == True:
-                            diff = numSweets - 30
-                            step = diff + 1
-                        else:
-                            step = Step(player)
-                    sum1 += step
-                    player = 1
-                    j += 1
-            else:
-                if player == 1:
-                    player = 2
-                    step = Step(player)
-                    sum2 += step
-                else:
-                    if numSweets > 29 and numSweets < 58:  # оста
-                        player = 1
-                        if bot == True:
-                            diff = numSweets - 30
-                            step = diff+1  # Останется 29
-                        else:
-                            step = Step(player)
-                        sum1 += step
-                    j += 1
-        else:
-            if player == 1:
-                player = 2
-                step = Step(player, numSweets)
-                sum2 += step
-            else:
-                player = 1
-                if bot == True:
-                    step = numSweets  # Останется 29\
-                else:
-                    step = Step(player, numSweets)
-                sum1 += step
-
-        counterSteps += 1
-        numSweets -= step  # 58
-        print(counterSteps, "ход. Игрок", player, "взял конфет:", step)
-        print("Осталось конфет:", numSweets)
-
-    print("Игрок 1 взял конфет:", sum1)
-    print("Игрок 2 взял конфет:", sum2)
-    return player
+# Создайте программу для игры в ""Крестики-нолики"".
 
 
-def Game(numSweets, player, bot=True):
-    counter = 0
-    i = numSweets
-    sum1 = 0
-    sum2 = 0
-    maxSweets = 28
-    threshold = 56
-    while i > 0:
-        counter += 1
-        if player == 1:
-            if bot == True:
-                step = random.randint(1, maxSweets)
-            else:
-                step = Step(player)
-        else:
-            step = Step(player)
-        i -= step
-        print(counter, "ход. Игрок", player, "взял конфет:", step)
-        if player == 1:
-            sum1 += step
-        else:
-            sum2 += step
-        print("Осталось конфет:", i)
-        diff = i-threshold
-        if diff <= maxSweets and i > threshold:
-            return FinishGame(i, player, counter, sum1, sum2, bot)
-        else:
-            player = 1 if player == 2 else 2
-        print(sum2) if player == 1 else print(sum1)
-
-    # return player
+row = lambda:  ["|   ", "|   |", "   |"]
 
 
-player = random.randint(1, 2)
-print(Game(300, player, True), "winner")
+matrix = [row(), row(), row()]
+# print(matrix)
 
-print("Первый ход сделал игрок №", player)
+
+def CheckWin(coord, player):
+    if player == True:
+        player = 1
+    else:
+        player = 2
+    comb1 = [[0, 0], [0, 1], [0, 2]]
+    comb2 = [[0, 0], [1, 1], [2, 2]]
+    comb3 = [[0, 0], [1, 0], [2, 0]]
+    comb4 = [[2, 0], [1, 1], [0, 2]]
+    k = 0
+    while k < 3:
+        j = 0
+        for i in coord:
+            if i in map(lambda x: [x[0]+k, x[1]], comb1):
+                j += 1
+        if j == 3:
+            return player
+        j = 0
+        for i in coord:
+            if i in comb2:
+                j += 1
+        if j == 3:
+            return player
+        j = 0
+        for i in coord:
+            if i in comb4:
+                j += 1
+        if j == 3:
+            return player
+        j = 0
+        for i in coord:
+            if i in map(lambda x: [x[0], x[1]+k], comb3):
+                j += 1
+        if j == 3:
+            return player
+        k += 1
+    return 0
+
+
+def InitField(matrix, x, y, value):
+    if y == 0:
+        matrix[x][y] = f'| {value} '
+    elif y == 1:
+        matrix[x][y] = f'| {value} |'
+    elif y == 2:
+        matrix[x][y] = f' {value} |'
+    i, j = [0, 0]
+    while i < len(matrix):
+        j = 0
+        while j < len(matrix[i]):
+            print(f'{matrix[i][j]}', end='')
+            j += 1
+        print("\n-------------")
+        i += 1
+    return matrix
+
+
+player = False
+i = 0
+firstPlayer = []
+secondPlayer = []
+while (i < 9):
+    player = not player
+    if player == True:
+        symbol = 'crosses'
+    else:
+        symbol = 'nulls'
+    x = int(input(f'{symbol} row: '))-1
+    y = int(input(f'{symbol} col: '))-1
+    if [x,y] in firstPlayer or [x,y] in secondPlayer:
+        continue
+    if player:
+        firstPlayer.append([x, y])
+        matrix = InitField(matrix, x, y, 'x')
+        if CheckWin(firstPlayer, player) > 0:
+            print("Crosses win!")
+            break
+    else:
+        secondPlayer.append([x, y])
+        matrix = InitField(matrix, x, y, '0')
+        if CheckWin(secondPlayer, player) > 0:
+            print("Nulls win!")
+            break
+
+    i += 1
+if i == 9:
+    print("Draw!")
---
+++ sweets.py
@@ -0,0 +1,129 @@
+import random
+# Создайте программу для игры с конфетами человек против человека.
+
+def Step(player, limit=28):
+    step = int(input(f"Игрок {player}, Ваш ход: "))
+    while step <= 0 or step > limit:
+        step = int(input(f"Игрок {player}, Ваш ход: "))
+    return step
+
+
+def FinishGame(numSweets, player, counterSteps, sum1, sum2, bot=True):
+    print("Final")
+    threshold = 56
+    maxSweets = 28
+    while numSweets >= threshold+2:
+        if player == 2:
+            player = 1
+            if bot == True:
+                diff = numSweets - threshold
+                step = diff-2
+            else:
+                step = Step(player)
+            sum1 += step
+        else:
+            player = 2
+            step = Step(player)
+            sum2 += step
+        counterSteps += 1
+        numSweets -= step  # <=58
+        print(counterSteps, "ход. Игрок", player, "взял конфет:", step)
+        print("Осталось конфет:", numSweets)
+    j = 0
+    while numSweets > 0:
+        if j == 0:
+            if numSweets >= threshold+2:  # >=58
+                if player == 1:
+                    player = 2
+                    step = Step(player)
+                    sum2 += step
+                else:
+                    if step == maxSweets:  #
+                        if bot == True:
+                            step = 1  # Останется 29
+                        else:
+                            step = Step(player)
+                    elif step < maxSweets:
+                        if bot == True:
+                            diff = numSweets - 30
+                            step = diff + 1
+                        else:
+                            step = Step(player)
+                    sum1 += step
+                    player = 1
+                    j += 1
+            else:
+                if player == 1:
+                    player = 2
+                    step = Step(player)
+                    sum2 += step
+                else:
+                    if numSweets > 29 and numSweets < 58:  # оста
+                        player = 1
+                        if bot == True:
+                            diff = numSweets - 30
+                            step = diff+1  # Останется 29
+                        else:
+                            step = Step(player)
+                        sum1 += step
+                    j += 1
+        else:
+            if player == 1:
+                player = 2
+                step = Step(player, numSweets)
+                sum2 += step
+            else:
+                player = 1
+                if bot == True:
+                    step = numSweets  # Останется 29\
+                else:
+                    step = Step(player, numSweets)
+                sum1 += step
+
+        counterSteps += 1
+        numSweets -= step  # 58
+        print(counterSteps, "ход. Игрок", player, "взял конфет:", step)
+        print("Осталось конфет:", numSweets)
+
+    print("Игрок 1 взял конфет:", sum1)
+    print("Игрок 2 взял конфет:", sum2)
+    return player
+
+
+def Game(numSweets, player, bot=True):
+    counter = 0
+    i = numSweets
+    sum1 = 0
+    sum2 = 0
+    maxSweets = 28
+    threshold = 56
+    while i > 0:
+        counter += 1
+        if player == 1:
+            if bot == True:
+                step = random.randint(1, maxSweets)
+            else:
+                step = Step(player)
+        else:
+            step = Step(player)
+        i -= step
+        print(counter, "ход. Игрок", player, "взял конфет:", step)
+        if player == 1:
+            sum1 += step
+        else:
+            sum2 += step
+        print("Осталось конфет:", i)
+        diff = i-threshold
+        if diff <= maxSweets and i > threshold:
+            return FinishGame(i, player, counter, sum1, sum2, bot)
+        else:
+            player = 1 if player == 2 else 2
+        print(sum2) if player == 1 else print(sum1)
+
+    # return player
+
+
+player = random.randint(1, 2)
+print(Game(300, player, True), "winner")
+
+print("Первый ход сделал игрок №", player)
